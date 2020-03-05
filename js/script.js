0f03991b8e59971f095c0da6ae5a27710bc4f600
Nikita <Nikitaodessapc@gmail.com>
2020-03-05 07:54:44 +0200

    v2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+$('.menu-btn').on('click', function (e) {
+  e.preventDefault();
+  $('.menu').toggleClass('menu_active');
+  $(this).toggleClass('menu-btn_active');
+  $('.content').toggleClass('content_active');
+})
+// $('.menu-btn').on('click', function (e) {
+//   e.preventDefault();
+//   $(this).toggleClass('menu-btn_active');
+//   $('.menu').toggleClass('menu_active');
+// });
